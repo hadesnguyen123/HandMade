24c75207ee4debe56ec93a4269d1f3f6424db4ee
Nguyễn Hoàng <96653565+hadesnguyen123@users.noreply.github.com>
2024-03-19 07:20:59 +0700

    add new

--- src/Component/NewCollections/NewCollections.jsx
+++ src/Component/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ import { Item } from '../Item/Item'
 export const NewCollections = () => {
     return (
         <div className='new-collections'>
-            <h1>NEW COLLECTIONS</h1>
+            <h1>New Product</h1>
             <hr />
             <div className="collections">
                 {new_collection.map((item, index) => {
